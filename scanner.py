e561fb579520383a6c524b2f7c0a23bdccf5d60d
yanshikalobin <yanshika.lobin@umail.uom.ac.mu>
2026-06-04 17:52:45 +0400

    Added hostname resolution, service detection, risk scoring and improved CSV export

--- scanner.py
+++ scanner.py
@@ -1,10 +1,37 @@
 import socket
 import time
+from datetime import datetime
 import csv
 import ipaddress
 import threading
 from queue import Queue
 
+#Service Detection Dictionary
+COMMON_SERVICES = {
+    20: "FTP Data",
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    443: "HTTPS",
+    3306: "MySQL",
+    3389: "RDP"
+
+}
+
+# HIGH RISK PORTS
+HIGH_RISK_PORTS = [21, 23, 445, 3389]
+
+#Hostname Resolution
+def get_hostname(ip):
+    try:
+        return socket.gethostbyaddr(ip)[0]
+    except:
+        return "Unknown"
 
 # IP VALIDATION (SAFETY LAYER)
 def validate_ip(ip):
@@ -34,16 +61,35 @@ def parse_ports(port_input):
 # PORT SCANNER FUNCTION
 def scan_port(ip, port, results):
     try:
+        risk = "Low"
+
+        if port in HIGH_RISK_PORTS:
+            risk = "High"
+
+        service = COMMON_SERVICES.get(port, "Unknown")
+
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(0.5)
 
         result = sock.connect_ex((ip, port))
 
         if result == 0:
-            print(f"[+] Port {port} OPEN")
-            results.append({"port": port, "status": "open"})
+            print(f"[+] Port {port} OPEN ({service}) Risk: {risk}")
+
+            results.append({
+                "port": port,
+                "status": "open",
+                "service": service,
+                "risk": risk
+            })
+
         else:
-            results.append({"port": port, "status": "closed"})
+            results.append({
+                "port": port,
+                "status": "closed",
+                "service": service,
+                "risk": risk
+            })
 
         sock.close()
 
@@ -82,13 +128,14 @@ def run_scan(ip, ports):
 
 #CSV Export
 def save_results(results, filename="scan_results.csv"):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(filename, "w", newline="") as file:
         writer = csv.writer(file)
 
-        writer.writerow(["Port", "Status"])
+        writer.writerow(["Timestamp","Port", "Status", "Service", "Risk"])
 
         for r in results:
-            writer.writerow([r["port"], r["status"]])
+            writer.writerow([timestamp,r["port"], r["status"], r["service"], r["risk"]])
 
     print(f"\n[+] Results saved to {filename}")
 
@@ -100,6 +147,8 @@ def timed_scan(ip, ports):
 
     end = time.time()
 
+    hostname = get_hostname(ip)
+    print(f"\nHostname: {hostname}")
     print(f"\nScan completed in {end - start:.2f} seconds")
 
     return results
